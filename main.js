82984682d4a74f7ea50b78ecc6f7942ef63b6e08
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-23 19:40:05 +1000

    Display a window and a jaunty greeting

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 /* main.js */
 
-console.log("Hello from Svalbard ^___^")
+const { app, BrowserWindow } = require('electron');
+
+console.log("Hello from Svalbard ^___^");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800
+    });
+
+    win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
