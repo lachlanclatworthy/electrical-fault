bf18666de0d468c5b03ee18fae81497cdb4ba764
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-23 21:14:16 +1000

    Farewell on all windows closed

--- main.js
+++ main.js
@@ -22,3 +22,8 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 });
+
+app.on('window-all-closed', () => {
+    console.log("Farewell from Svalbard x___x");
+    app.quit();
+})
